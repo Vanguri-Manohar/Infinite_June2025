91c0f82bbbd49ce77c65f86875e8185c0e1cede2
Vanguri-Manohar <vangurissmanohar@gmail.com>
2025-08-23 21:34:36 +0530

    MVC-Assignment

---
+++ MVC_Assignment/MVC_Assignment/MVC_Assignment/Migrations/202508231558306_SyncModel.cs
@@ -0,0 +1,20 @@
+namespace MVC_Assignment.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SyncModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Contacts", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Contacts", "LastName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Contacts", "LastName", c => c.String());
+            AlterColumn("dbo.Contacts", "FirstName", c => c.String());
+        }
+    }
+}
